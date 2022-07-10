f4b3bc8189362df19293874be271dc5df03db4a7
Jaehyeon Oh <72315719+JaeHyeon-Oh@users.noreply.github.com>
2022-07-10 15:44:23 +0900

    Update setup.py
    Modify this file

--- setup.py
+++ setup.py
@@ -1,111 +1,85 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from pathlib import Path
 
-import os
-import re
-import shutil
-import sys
+from setuptools import find_packages, setup
 
-from setuptools import setup
+extras_require = {
+    "test": [
+        "cryptography",
+        "pytest-cov",
+        "pytest-django",
+        "pytest-xdist",
+        "pytest",
+        "tox",
+    ],
+    "lint": [
+        "flake8",
+        "pep8",
+        "isort",
+    ],
+    "doc": [
+        "Sphinx>=1.6.5,<2",
+        "sphinx_rtd_theme>=0.1.9",
+    ],
+    "dev": [
+        "pytest-watch",
+        "wheel",
+        "twine",
+        "ipython",
+    ],
+    "python-jose": [
+        "python-jose==3.3.0",
+    ],
+    "crypto": [
+        "cryptography>=3.3.1",
+    ],
+}
 
-
-def get_version(package):
-    """
-    Return package version as listed in `__version__` in `init.py`.
-    """
-    with open(os.path.join(package, '__init__.py'), 'rb') as init_py:
-        src = init_py.read().decode('utf-8')
-        return re.search("__version__ = ['\"]([^'\"]+)['\"]", src).group(1)
-
-
-name = 'djangorestframework-jwt'
-version = get_version('rest_framework_jwt')
-package = 'rest_framework_jwt'
-description = 'JSON Web Token based authentication for Django REST framework'
-url = 'https://github.com/jpadilla/django-rest-framework-jwt'
-author = 'Jose Padilla'
-author_email = 'jpadilla@getblimp.com'
-license = 'MIT'
-install_requires = [
-    'PyJWT>=1.5.2,<2.0.0',
-]
-
-
-def read(*paths):
-    """
-    Build a file path from paths and return the contents.
-    """
-    with open(os.path.join(*paths), 'r') as f:
-        return f.read()
-
-
-def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
-    return [dirpath
-            for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-
-def get_package_data(package):
-    """
-    Return all files under the root package, that are not in a
-    package themselves.
-    """
-    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
-            for dirpath, dirnames, filenames in os.walk(package)
-            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-    filepaths = []
-    for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename)
-                          for filename in filenames])
-    return {package: filepaths}
-
-
-if sys.argv[-1] == 'publish':
-    if os.system('pip freeze | grep twine'):
-        print('twine not installed.\nUse `pip install twine`.\nExiting.')
-        sys.exit()
-    os.system('python setup.py sdist bdist_wheel')
-    os.system('twine upload dist/*')
-    shutil.rmtree('dist')
-    shutil.rmtree('build')
-    shutil.rmtree('djangorestframework_jwt.egg-info')
-    print('You probably want to also tag the version now:')
-    print("  git tag -a {0} -m 'version {0}'".format(version))
-    print('  git push --tags')
-    sys.exit()
+extras_require["dev"] = (
+    extras_require["dev"]
+    + extras_require["test"]
+    + extras_require["lint"]
+    + extras_require["doc"]
+    + extras_require["python-jose"]
+)
 
 
 setup(
-    name=name,
-    version=version,
-    url=url,
-    license=license,
-    description=description,
-    long_description=read('README.rst'),
-    author=author,
-    author_email=author_email,
-    packages=get_packages(package),
-    package_data=get_package_data(package),
-    install_requires=install_requires,
+    name="djangorestframework_simplejwt",
+    use_scm_version={"version_scheme": "post-release"},
+    setup_requires=["setuptools_scm"],
+    url="https://github.com/jazzband/djangorestframework-simplejwt",
+    license="MIT",
+    description="A minimal JSON Web Token authentication plugin for Django REST Framework",
+    long_description=Path("README.rst").read_text(encoding="utf-8"),
+    author="David Sanders",
+    author_email="davesque@gmail.com",
+    install_requires=[
+        "django",
+        "djangorestframework",
+        "pyjwt>=1.7.1,<3",
+    ],
+    python_requires=">=3.7",
+    extras_require=extras_require,
+    packages=find_packages(exclude=["tests", "tests.*", "licenses", "requirements"]),
+    include_package_data=True,
+    zip_safe=False,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Internet :: WWW/HTTP',
-    ]
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
 )
